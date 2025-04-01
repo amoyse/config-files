a4e7f006f20090f10bc61d60ced8a8f27b0b9a94
amoyse <securemoyse127@protonmail.com>
2025-04-01 20:28:26 +0100

    removed ltex grammar warnings

--- lua-nvim-config/lua/config/lsp.lua
+++ lua-nvim-config/lua/config/lsp.lua
@@ -127,7 +127,19 @@ require('lspconfig').pylsp.setup {
 require('lspconfig').ltex.setup {
   settings = {
     ltex = {
-      language = "en-GB"
+      language = "en-GB",
+      disabledRules = {
+          ["en"]    = { "MORFOLOGIK_RULE_EN"    },
+          ["en-AU"] = { "MORFOLOGIK_RULE_EN_AU" },
+          ["en-CA"] = { "MORFOLOGIK_RULE_EN_CA" },
+          ["en-GB"] = { "MORFOLOGIK_RULE_EN_GB" },
+          ["en-NZ"] = { "MORFOLOGIK_RULE_EN_NZ" },
+          ["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
+          ["en-ZA"] = { "MORFOLOGIK_RULE_EN_ZA" },
+          ["es"]    = { "MORFOLOGIK_RULE_ES"    },
+          ["it"]    = { "MORFOLOGIK_RULE_IT_IT" },
+          ["de"]    = { "MORFOLOGIK_RULE_DE_DE" },
+      },
     }
   }
 }
